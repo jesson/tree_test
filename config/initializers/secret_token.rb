7fdaf1a7110626729817e28b796165ed1c6fa01d
Oleg A. Bavaev <jesoba7@gmail.com>
2012-08-11 19:58:03 +1100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TreeTest::Application.config.secret_token = '8e6ec0322dd7f56702cab42b15a39d816b09f15d7495b10ec17b1635bff82fc64ff5f514dada6b53f7b1e5cd95eabc64be985a86e7d684049300facf19d57431'
